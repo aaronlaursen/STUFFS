69eb9881b8ccbddde99db4df19f6c5a8a9efbd90
aaronlaursen <aaronlaursen@gmail.com>
2013-08-21 15:34:16 -0500

    rewrite, working tags, vreatable files (not read/write yet)

--- SPOTFS.py
+++ SPOTFS.py
@@ -1,111 +1,340 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
-import sqlalchemy as sql
-import os
-from sqlalchemy.orm import sessionmaker
-Session = sessionmaker()
-
-DBPATH=os.getcwd()+"/tag.db"
+#spotfs rewrite**2
 
 
+from sqlalchemy import Table, Column, Integer, ForeignKey, BLOB, \
+        Boolean, String, create_engine, MetaData
+from sqlalchemy.orm import relationship, backref, sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+from time import time
+from stat import S_IFDIR, S_IFLNK, S_IFREG
+from hashlib import md5
+from fuse import Operations, LoggingMixIn, FUSE, FuseOSError
+from sys import argv
+from errno import ENOENT
 
-def main():
-    db = sql.create_engine('sqlite://'+DBPATH)
-    db.echo = False
-    Session.configure(bind=db)
-    metadata = sql.MetaData(db)
-    tables=initDB(metadata)
-    return
 
-def initDB(metadata):
-    tables={}
-    tables['tags'] = sql.Table(
-            'tags'
-            , metadata
-            , sql.Column('id', sql.Integer, primary_key=True)
-            , sql.Column('tag', sql.String)
-            , sql.Column('num_uses', sql.Integer)
-            , sql.Column('hidden', sql.Boolean)
+#database stuff
+DBPATH="fs.db"
+db = create_engine('sqlite:///'+DBPATH)
+db.echo = False
+Base = declarative_base(metadata=MetaData(db))
+Session = sessionmaker(bind=db)
+#session=Session()
+
+Table('use'
+    , Base.metadata
+    , Column('file_id', Integer, ForeignKey('files.id'))
+    , Column('tag_id', Integer, ForeignKey('tags.id'))
+)
+
+class File(Base):
+    __tablename__ = 'files'
+    def __init__(self, data=None):
+        self.data=data
+    id = Column(Integer, primary_key=True)
+    data = Column(BLOB)
+    attrs = Column(String)
+    name = Column(String)
+    tags = relationship("Tag"
+                    , secondary="use"
+                    , backref=backref("files", collection_class=set)
+                    , collection_class=set
+                    )
+
+class Tag(Base):
+    __tablename__ = 'tags'
+    def __init__(self, txt):
+        self.name=txt
+    id = Column(Integer, primary_key=True)
+    name = Column(String, nullable=False, unique=True)
+    attrs = Column(String)
+
+Base.metadata.create_all()
+
+def mkfile(name, session, mode=0o770, tags=None):
+    f = File()
+    session.add(f)
+    if tags !=None:
+        f.tags |= set(tags)
+    a = {'st_mode':(S_IFREG | mode)
+                , 'st_nlink':1
+                , 'st_size':0
+                , 'st_ctime':time()
+                , 'st_mtime':time()
+                , 'st_atime':time()
+                , 'uid':0
+                , 'gid':0
+                }
+    f.attrs = convertAttr(a)
+    f.name=name
+    return f
+
+def mktag(txt, session, mode=0o777):
+    t=Tag(txt)
+    session.add(t)
+    a = {'st_mode':(S_IFDIR | mode)
+                , 'st_nlink':1
+                , 'st_size':0
+                , 'st_ctime':time()
+                , 'st_mtime':time()
+                , 'st_atime':time()
+                , 'uid':0
+                , 'gid':0
+                }
+    t.attrs = convertAttr(a)
+    return t
+
+def convertAttr(attrs):
+    attrdata=( ('st_mode',int)
+            , ('st_nlink',int)
+            , ('st_size',int)
+            , ('st_ctime',float)
+            , ('st_mtime',float)
+            , ('st_atime',float)
+            , ('uid', int)
+            , ('gid', int)
             )
-    tables['files'] = sql.Table(
-            'files'
-            , metadata
-            , sql.Column('id', sql.Integer, primary_key=True)
-            , sql.Column('data', sql.Binary)
-            )
-    tables['uses'] = sql.Table(
-            'uses'
-            , metadata
-            , sql.Column('id', sql.Integer, primary_key=True)
-            , sql.Column('file_id', sql.Integer, sql.ForeignKey('files.id'))
-            , sql.Column('tag_id', sql.Integer, sql.ForeignKey('tag.id'))
-            )
-    for t in tables:
-        t.create()
+    if type(attrs) == type(dict()):
+        s=''
+        for i in range(len(attrdata)):
+            s+=str(attrs[attrdata[i][0]])
+            s+=','
+        return s[:-1]
+    if type(attrs) == type(''):
+        attrs=attrs.split(',')
+        d={attrdata[i][0]:attrdata[i][1](attrs[i]) for i in range(len(attrdata))}
+        return d
+    return None
 
-    sql.mapper(Tag, tables['tags'])
-    sql.mapper(File, tables['files'])
-    sql.mapper(Use, tables['uses'])
-    return tables
+def getIdFromString(s):
+    t={'%':Tag,'@':File}
+    if len(s) <3: return 0, File
+    if s[-1] not in ('%','@'):
+        return 0, File
+    if len(s.split(s[-1]))<3:
+        return 0, File
+    i=s.split(s[-1])[-2]
+    if not i.isdigit():
+        return 0, File
+    return int(i), t[s[-1]]
 
-class Tag(object):
-    def __init__(self, tag=None, num_uses=0):
-        self.tag=tag
-        self.num_uses=num_uses
-class File(object):
-    def __init__(self, location=None):
-        self.location=location
-class Use(object):
-    def __init__(self, fileID=None, tagID=none):
-        self.file_id=fileID
-        self.tag_id=tagID
+def genDisplayName(obj):
+    if obj.__tablename__=='files':
+        name=obj.name
+        s='@'
+    elif obj.__tablename__=='tags':
+        name=obj.name
+        s='%'
+    name += s + str(obj.id) +s
+    return name
 
-def insert(data,table):
-    table.insert().execute(data)
-    return data['id']
+def getByID(id_, session, typ=File):
+    return session.query(typ).get(int(id_))
 
-def addTag(tag, tag_table, num_uses=0):
-    data={'tag': tag, 'num_uses':0}
-    insert(data, tag_table)
+def getFilesByTags(tags,session):
+    q=session.query(File)
+    for t in tags:
+        q=q.filter(File.tags.contains(t))
+    return q.all()
 
-def addFile(contents, file_table):
-    data={'data':contents,}
-    insert(data, file_table)
+def getTagsByTxts(txts,session):
+    q=session.query(Tag).filter(Tag.name.in_(txts))
+    return q.all()
 
-def addUse(FileID, TagID, use_table):
-    data={'file_id':FileID, 'tag_id':TagID}
-    insert(data, use_table)
+def getFilesByTagTxts(txts,session):
+    tags=getTagsByTxts(txts,session)
+    return getFilesByTags(tags,session)
 
-def getByID(id_, table):
-    s=sql.selectfirst(table.c.id == id_)
-    return s.execute()
+def getTagsByFiles(files):
+    tags=set()
+    for f in files:
+        tags |= f.tags
+    return tags
 
-def deleteByID(id_, table):
-    s=sql.delete(table.c.id==id_)
-    s.execute()
-    return
+def getTagsFromPath(path,session):
+    print("----------------------")
+    print("%"+path+"%")
+    tagnames=set(path.split('/')).discard('')
+    print(tagnames)
+    print("----------------------")
+    if type(tagnames)==type(None): return set()
+    if len(tagnames)==0: return set()
+    idtags=set()
+    for t in tagnames:
+        id_,typ = getIdFromString(t)
+        tag=getByID(id_, session, Tag)
+        if tag: idtags.add(tag)
+    nametags=set(getTagsByTxts(tagnames,session))
+    return idtags | nametags
 
-def deleteUseClean(id_, tables):
-    pass
+def getEndTagFromPath(path,session):
+    #if path=='/': return None
+    path=path.strip('/')
+    path=path.split("/")
+    tagname=path[-1]
+    if tagname=='': return None
+    id_, typ = getIdFromString(tagname)
+    tag=getByID(id_, session, Tag)
+    if tag: return tag
+    return getTagsByTxts(tagname,session)[0]
 
-def dincNum_Uses(id_,table):
-    session = Session()
-    tag = session.query(Tag).selectfirst(table.c.id==id_)
-    tag.num_uses -= 1
-    new_num=tag.num_uses
-    session.flush()
-    return new_num
+def getFileByNameAndTags(name,tags,session):
+    q=session.query(File).filter(File.name==name)
+    for t in tags:
+        q=q.filter(File.tags.contains(t))
+    return q.first()
 
-def deleteUsesByFileID(file_id, uses_table):
-    s=sql.delete(uses_table.c.file_id == file_id)
-    s.execute()
-    return
+def getFileFromPath(path,session):
+    path=path.strip('/')
+    pieces=path.split('/')
+    fstring=pieces[-1]
+    fid,typ=getIdFromString(fstring)
+    f=getByID(fid, session, File)
+    if f: return f
+    path = ""
+    for p in pieces[:-1]: path +=p+"/"
+    return getFileByNameAndTags(fstring,getTagsFromPath(path,session),session)
+
+def getSubByTags(tags,session):
+    if len(tags)==0:return genEverything(session)
+    subfiles=set(getFilesByTags(tags,session))
+    subtags=getTagsByFiles(subfiles)
+    return subfiles | subtags
+
+def genSub(path,session):
+    tags=getTagsFromPath(path,session)
+    sub=getSubByTags(tags,session)
+    print("############")
+    print(sub)
+    print("############")
+    return sub
+
+def genSubDisplay(path,session):
+    sub=genSub(path,session)
+    return [genDisplayName(x) for x in sub]
+
+def getAttrByObj(obj):
+    return convertAttr(obj.attrs)
+
+def getObjByPath(path,session):
+    if path[-1]=='/':
+        return getEndTagFromPath(path,session)
+    objname=path.split('/')[-1]
+    print("============")
+    print(objname)
+    print(getIdFromString(objname))
+    print("============")
+    id_, typ = getIdFromString(objname)
+    obj = getByID(id_, session,typ)
+    if obj: return obj
+    pathpieces=path.rsplit('/',1)
+    opts=genSub(pathpieces[0]+'/',session)
+    for o in opts:
+        if o.name==pathpieces[1]: return o
+    return getFileFromPath(path,session)
+
+def genEverything(session):
+    stuff=set()
+    q=session.query(File)
+    stuff |= set(q.all())
+    q=session.query(Tag)
+    stuff |= set(q.all())
+    print("------stuff:",stuff)
+    return stuff
+
+def genDisplayEverything(session):
+    stuff=genEverything(session)
+    return [genDisplayName(obj) for obj in stuff]
+
+def getAttrByPath(path,session):
+    obj=getObjByPath(path,session)
+    if not obj: return None
+    return getAttrByObj(obj)
+
+#fuse stuff
+class SpotFS(LoggingMixIn, Operations):
+    def __init__(self):
+        self.fd=0
+
+    def getattr(self, path, fh=None):
+        print("getattr:", path, fh)
+        session=Session()
+        if path=='/': return {'st_mode':(0o777)
+                , 'st_nlink':1
+                , 'st_size':0
+                , 'st_ctime':time()
+                , 'st_mtime':time()
+                , 'st_atime':time()
+                , 'uid':0
+                , 'gid':0
+                }
+        attr=getAttrByPath(path,session)
+        print("+++++++++")
+        print(attr)
+        print("+++++++++")
+        if not attr:
+            raise FuseOSError(ENOENT)
+        return attr
+
+    def mkdir(self,path,mode):
+        session=Session()
+        path=path.strip('/')
+        path=path.split('/')
+        txt=path[-1]
+        mktag(txt, session, mode)
+        session.flush()
+        session.commit()
+
+    def readdir(self,path,fh=None):
+        session=Session()
+        if path=='/': return ['.','..']+genDisplayEverything(session)
+        return ['.','..']+genSubDisplay(path,session)
+
+    def chmod(self, path, mode):
+        session=Session()
+        obj=getObjByPath(path,session)
+        if not obj: return
+        attrs=convertAttr(obj.attrs)
+        attrs['st_mode'] |=mode
+        obj.attrs=convertAttr(attrs)
+        session.flush()
+        session.commit()
+        return 0
+
+    def chown(self, path,uid,gid):
+        session=Session()
+        obj=getObjByPath(path,session)
+        if not obj: return
+        attrs=convertAttr(obj.attrs)
+        attrs['uid']=uid
+        attrs['gid']=gid
+        obj.attrs=convertAttr(attrs)
+        session.add(obj)
+        session.flush()
+        session.commit()
+
+    def create(self,path,mode):
+        print("creat reached:",path,mode)
+        session=Session()
+        tpath, name = path.rsplit("/",1)
+        tags=getTagsFromPath(path,session)
+        mkfile(name,session,tags=tags)
+        session.commit()
+        self.fd +=1
+        return self.fd
+
+    def open(self,path,flags):
+        print("open reached:",path,flags)
+        self.fd+=1
+        return self.fd
 
 
-def deleteFileClean(id_,tables):
-    deleteUsesByFileID(id_,tables['uses'])
 
+if __name__ == "__main__":
+    if len(argv) != 2:
+        print('usage: %s <mountpoint>' % argv[0])
+        exit(1)
+    fuse = FUSE(SpotFS(), argv[1], foreground=True)
 
-
-
-main()
